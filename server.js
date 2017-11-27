b73eeca5a77baa7cd360b34c5f2a69771824a366
DamolaAdewunmi <damolasd@gmail.com>
2017-11-27 08:28:27 +0100

    Added changes to the serverjs file

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
+var Projects = require('./PrplanModel/model.js')
 var express = require('express');
 var app = express();
+mongoose = require('mongoose');
+var dbName = 'Prplan';
+var connectionString = 'mongodb://localhost:27017/' + dbName
 
+mongoose.connect(connectionString);
 port  =process.env.PORT || 3000;
 app.listen(port);
 
+
+app.get('/Projects', function(req, res) { 
+	Projects.find(function(err,project) {
+		if (err) {
+			return res.send(err);
+		}
+		res.json(project)
+	});
+
+});
+
+app.get('/Projects/ProjectId/Items', function(req, res) {
+
+})
+
+
+app.post('/Projects', function(req, res){
+	var project = new Projects(req.body);
+
+	project.save(function(err) {
+		if (err) {
+			return res.send(err);
+		}
+		res.send({message: 'Project added'});
+	});
+
+});
+
+app.put('/Projects', function (req, res) {
+	// body...
+})
 console.log('todo list RESTful API server started on: '+ port);
